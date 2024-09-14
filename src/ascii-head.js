4c92acaee28286f9a2e4d2b3447b57e46db0dd74
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-09-14 15:07:57 -0500

    Add ASCII self-portrait, three.js

---
+++ src/ascii-head.js
@@ -0,0 +1,79 @@
+import * as THREE from 'three';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+// -----------
+// Scene
+// -----------
+const scene = new THREE.Scene();
+const canvas = document.getElementById("ascii-head");
+const gltfLoader = new GLTFLoader();
+let jacobModel, camera, renderer, effect;
+gltfLoader.load(
+    '/jacob.glb',
+    (gltf) =>
+    {
+        console.log(gltf)
+        scene.add(gltf.scene);
+        jacobModel = gltf.scene;
+        camera.lookAt(jacobModel.position);
+        jacobModel.position.set(0,-3.25,0);
+    }
+);
+const pointLight1 = new THREE.PointLight( 0xffffff, 2, 0, 0 );
+pointLight1.position.set( 500, 500, 500 );
+scene.add( pointLight1 );
+const pointLight2 = new THREE.PointLight( 0xffffff, 1, 0, 0 );
+pointLight2.position.set( - 500, - 500, - 500 );
+scene.add( pointLight2 );
+
+// -----------
+// Aspect Ratio and Camera
+// -----------
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight
+};
+camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.set(-4.5,2,7);
+
+// -----------
+// Renderer
+// -----------
+renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+renderer.setSize( sizes.width, sizes.height );
+renderer.setAnimationLoop( animate );
+
+// ASCII Effect
+effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: false } );
+effect.setSize( sizes.width, sizes.height );
+effect.domElement.style.color = 'black';
+
+canvas.parentNode.replaceChild(effect.domElement, canvas);
+
+// -----------
+// Window Resize
+// -----------
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(sizes.width, sizes.height);
+    effect.setSize(sizes.width, sizes.height);
+});
+
+// Animation
+const clock = new THREE.Clock()
+
+function animate() {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    if(jacobModel) jacobModel.rotation.y = elapsedTime;
+
+    effect.render( scene, camera );
+
+};
