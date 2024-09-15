9c40d9e7353fd9afaea53dac0974054c7b7b7471
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-09-14 19:17:25 -0500

    Add drop shadow to ASCII, fix projection error

--- src/ascii-head.js
+++ src/ascii-head.js
@@ -9,72 +9,87 @@ const scene = new THREE.Scene();
 const canvas = document.getElementById("ascii-head");
 const gltfLoader = new GLTFLoader();
 let jacobModel, camera, renderer, effect;
-gltfLoader.load(
-    '/jacob.glb',
-    (gltf) =>
-    {
-        console.log(gltf)
-        scene.add(gltf.scene);
-        jacobModel = gltf.scene;
-        camera.lookAt(jacobModel.position);
-        jacobModel.position.set(-.25,-3.25,0);
-    }
-);
-const pointLight1 = new THREE.PointLight( 0xffffff, 4, 0, 0 );
-pointLight1.position.set( 500, 500, 500 );
-scene.add( pointLight1 );
-const pointLight2 = new THREE.PointLight( 0xffffff, 1, 0, 0 );
-pointLight2.position.set( - 500, - 500, - 500 );
-scene.add( pointLight2 );
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
 
 // -----------
 // Aspect Ratio and Camera
 // -----------
-const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight
-};
-camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.set(-4.5,2,7);
+camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 1, 1000);
+camera.position.set(-4.5, 2, 6);
 
 // -----------
 // Renderer
 // -----------
-renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
-renderer.setSize( sizes.width, sizes.height );
-renderer.setAnimationLoop( animate );
+renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // ASCII Effect
-effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: false } );
-effect.setSize( sizes.width, sizes.height );
+effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: false });
+effect.setSize(sizes.width, sizes.height);
 effect.domElement.style.color = 'black';
 effect.domElement.style.fontWeight = 'bolder';
+effect.domElement.style.textShadow = '1px 1px 2px black'
 
 canvas.parentNode.replaceChild(effect.domElement, canvas);
 
 // -----------
-// Window Resize
+// Load GLTF Model
 // -----------
-window.addEventListener('resize', () => {
+gltfLoader.load(
+    '/jacob.glb',
+    (gltf) => {
+        jacobModel = gltf.scene;
+        scene.add(jacobModel);
+        camera.lookAt(jacobModel.position);
+        jacobModel.position.set(-.25, -3.25, 0);
+        onResize();
+    }
+);
+
+// -----------
+// Lights
+// -----------
+const pointLight1 = new THREE.PointLight(0xffffff, 4, 0, 0);
+pointLight1.position.set(500, 500, 500);
+scene.add(pointLight1);
+
+const pointLight2 = new THREE.PointLight(0xffffff, 1, 0 ,0);
+pointLight2.position.set(-500, -500, -500);
+scene.add(pointLight2);
+
+// -----------
+// Window Resize Event
+// -----------
+window.addEventListener('resize', onResize);
+
+function onResize() {
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
-    
+
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
-    
+
     renderer.setSize(sizes.width, sizes.height);
     effect.setSize(sizes.width, sizes.height);
-});
+}
 
-// Animation
-const clock = new THREE.Clock()
+// -----------
+// Animation Loop
+// -----------
+const clock = new THREE.Clock();
 
 function animate() {
+    const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
+    if (jacobModel) {
+        jacobModel.rotation.y = elapsedTime * 0.65;
+    }
 
-    if(jacobModel) jacobModel.rotation.y = elapsedTime * 0.75;
+    effect.render(scene, camera);
+}
 
-    effect.render( scene, camera );
-
-};
+renderer.setAnimationLoop(animate);
